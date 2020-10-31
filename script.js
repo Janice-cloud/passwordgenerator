bb5db6620d4599854bc7b3f97576bd3ac8a037c3
Janice Leung <janice.leung2008@gmail.com>
2020-10-31 21:26:26 +1100

    up date javascript

--- script.js
+++ script.js
@@ -16,6 +16,64 @@ const randomFunc = {
   symbol: getRandomnSymbol,
 };
 
+// Generate event listener
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+
+  resultEl.innerText = generatePassword(
+    length,
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol
+  );
+});
+
+//Generate password function
+function generatePassword(length, lower, upper, number, symbol) {
+  // 1. Initialise password var
+  // 2. Filter out unchecked types
+  // 3. Loop over the length call generator function for each type
+  // 4. Add the final password to the password var and return.
+
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+
+  //console.log('typesCount', typesCount);
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
+  (
+    item => Object.values(item)[0]
+  );
+
+  //console.log('typesArr:', typesArr);
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      //console.log("funcName:", funcName);
+
+      generatedPassword += randomFunc[funcName]();
+
+    });
+  }
+  
+  const finalPassword = generatedPassword.slice(0, length);
+
+  //console.log(finalPassword)
+
+  return finalPassword
+}
+
 // Slider function
 
 selectValue.innerHTML = lengthEl.value;
@@ -45,4 +103,4 @@ function getRandomnSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(getRandomnSymbol());
+//console.log(getRandomnSymbol());
